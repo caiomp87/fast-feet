3de6817614813d47b333532569a219bc413e18c9
unknown <Administrator@TRJ3573393.wks.internal.timbrasil.com.br>
2020-02-14 01:22:01 -0300

    Iniciado o projeto fast feet

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'caio657804',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
